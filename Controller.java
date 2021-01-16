3084356dfc9f7c21191d99770c6911950694e200
ApprenticeOfEnder <perfknight@gmail.com>
2021-01-15 21:07:57 -0500

    Started implementing Controller class

--- Controller.java
+++ Controller.java
@@ -1,4 +1,22 @@
 public class Controller{
   private CourseCorrect model;
-  
+  private IOController io;
+
+  public Controller(String username){
+    model = new CourseCorrect(username);
+    io = new IOController(username, model);
+  }
+
+  public IOController getIO(){
+    return io;
+  }
+
+  public CourseCorrect getModel(){
+    return model;
+  }
+
+  public static void main(String[] args){
+    Controller app = new Controller("Ender");
+    app.getModel().menu(app.getIO());
+  }
 }
